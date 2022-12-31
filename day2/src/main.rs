9dd36fe2c4262e4860dc63efa4bbd4d93f704c42
Ivan Vasiunyk <i.vasiunyk@gmail.com>
2022-12-31 15:43:05 +0100

    Refactor day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,14 +3,14 @@ use std::path::PathBuf;
 
 use enum_iterator::{all, Sequence};
 
-use crate::Sign::*;
 use crate::GameResult::*;
+use crate::Sign::*;
 
 #[derive(Debug, PartialEq, Eq, Sequence)]
 enum Sign {
     Rock = 1,
     Paper = 2,
-    Scissors = 3
+    Scissors = 3,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -20,12 +20,16 @@ enum GameResult {
     Win = 6,
 }
 
+struct Rule {
+    pub beats: Sign,
+}
+
 fn char_to_sign(ch: char) -> Sign {
     match ch {
         'A' | 'X' => Rock,
         'B' | 'Y' => Paper,
         'C' | 'Z' => Scissors,
-        _ => panic!("Invalid input: {:?}", ch)
+        _ => panic!("Invalid input: {:?}", ch),
     }
 }
 
@@ -34,22 +38,29 @@ fn char_to_result(ch: char) -> GameResult {
         'X' => Defeat,
         'Y' => Draw,
         'Z' => Win,
-        _ => panic!("Invalid input: {:?}", ch)
+        _ => panic!("Invalid input: {:?}", ch),
+    }
+}
+
+fn rock_paper_scissors_rule_lookup(s: &Sign) -> Rule {
+    match s {
+        Rock => Rule { beats: Scissors },
+        Paper => Rule { beats: Rock },
+        Scissors => Rule { beats: Paper },
     }
 }
 
 fn rock_paper_scissors(player: &Sign, opponent: &Sign) -> GameResult {
     if player == opponent {
-        return Draw
+        return Draw;
     }
 
-    match player {
-        Rock if *opponent == Scissors => Win,
-        Rock => Defeat,
-        Paper if *opponent == Rock => Win,
-        Paper => Defeat,
-        Scissors if *opponent == Paper => Win,
-        Scissors => Defeat
+    let rule = rock_paper_scissors_rule_lookup(player);
+
+    if rule.beats == *opponent {
+        Win
+    } else {
+        Defeat
     }
 }
 
@@ -57,41 +68,45 @@ fn main() {
     let file_path = PathBuf::from("day2/resources/input.txt");
     let data_raw = fs::read_to_string(file_path).unwrap();
 
-    let total: u32 = data_raw.lines().map(|line| {
-        let arr: Vec<char> = line.split(" ").map(|s| s.chars().nth(0).unwrap()).collect();
-        assert_eq!(arr.len(), 2, "Invalid input {}", line);
+    let total: u32 = data_raw
+        .lines()
+        .map(|line| {
+            let arr: Vec<char> = line.split(" ").map(|s| s.chars().nth(0).unwrap()).collect();
+            assert_eq!(arr.len(), 2, "Invalid input {}", line);
 
-        let player = char_to_sign(arr[1]);
-        let opponent = char_to_sign(arr[0]);
+            let player = char_to_sign(arr[1]);
+            let opponent = char_to_sign(arr[0]);
 
-        let result = rock_paper_scissors(&player, &opponent);
+            let result = rock_paper_scissors(&player, &opponent);
 
-        player as u32 + result as u32
-    }).sum();
+            player as u32 + result as u32
+        })
+        .sum();
 
     println!("Game total(Answer #1): {}", total);
 
-    let total: u32 = data_raw.lines().map(|line| {
-        let arr: Vec<char> = line.split(" ").map(|s| s.chars().nth(0).unwrap()).collect();
-        assert_eq!(arr.len(), 2, "Invalid input {}", line);
+    let total: u32 = data_raw
+        .lines()
+        .map(|line| {
+            let arr: Vec<char> = line.split(" ").map(|s| s.chars().nth(0).unwrap()).collect();
+            assert_eq!(arr.len(), 2, "Invalid input {}", line);
 
-        let opponent = char_to_sign(arr[0]);
-        let result = char_to_result(arr[1]);
+            let opponent = char_to_sign(arr[0]);
+            let result = char_to_result(arr[1]);
 
-        let player = (|| {
-            for x in all::<Sign>() {
-                if rock_paper_scissors(&x, &opponent) == result {
-                    return x
+            let player = (|| {
+                for x in all::<Sign>() {
+                    if rock_paper_scissors(&x, &opponent) == result {
+                        return x;
+                    }
                 }
-            }
 
-            panic!("Should not end up here")
-        })();
+                panic!("Should not end up here")
+            })();
 
-        player as u32 + result as u32
-    }).sum();
+            player as u32 + result as u32
+        })
+        .sum();
 
     println!("Game total(Answer #2): {}", total);
 }
-
-
